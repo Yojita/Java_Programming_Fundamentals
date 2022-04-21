a850e429e3f76e3fdc9b1dfcd95c0dcbd81fced5
Yojita Rajput <yojita98@gmail.com>
2022-04-21 11:57:26 +0530

    Program to understand how implicit type casting with primitive type works.

---
+++ Java_Programming_Fundamentals/src/typeCasting/ImplicitTypeCasting.java
@@ -0,0 +1,12 @@
+package typeCasting;
+
+public class ImplicitTypeCasting {
+	public static void main(String[] args) {
+		int i = 300;
+		long l = i;
+		float f = l;
+		System.out.println("int value : " + i);
+		System.out.println("long value : " + l);
+		System.out.println("float value : " + f);
+	}
+}
